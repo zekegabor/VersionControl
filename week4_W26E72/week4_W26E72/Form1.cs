406262469c695bc266d29d3519f5f2993448fcfe
Zeke Gábor <zgabor2001@gmail.com>
2022-10-06 19:23:35 +0200

    Excel load commit

--- week4_W26E72/week4_W26E72/Form1.cs
+++ week4_W26E72/week4_W26E72/Form1.cs
@@ -6,15 +6,80 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
 using System.Windows.Forms;
 
 namespace week4_W26E72
 {
     public partial class Form1 : Form
     {
+        RealEstateEntities context = new RealEstateEntities();
+        List<Flat> Flats;
+
+        Excel.Application xlApp;
+        Excel.Workbook xlWb1;
+        Excel.Worksheet xlSheet;
+
         public Form1()
         {
             InitializeComponent();
+
+            LoadData();
+
+            CreateExcel();
+
+        }
+
+        private void CreateExcel()
+        {
+
+            try
+            {
+                xlApp = new Excel.Application();
+                xlWb1 = xlApp.Workbooks.Add(Missing.Value);
+                xlSheet = xlWb1.ActiveSheet;
+
+                CreateTable();
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                xlWb1.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWb1 = null;
+                xlApp = null;
+            }
+        }
+
+        private void CreateTable()
+        {
+            string[] headers = new string[]
+            {
+                     "Kód",
+                     "Eladó",
+                     "Oldal",
+                     "Kerület",
+                     "Lift",
+                     "Szobák száma",
+                     "Alapterület (m2)",
+                     "Ár (mFt)",
+                     "Négyzetméter ár (Ft/m2)"
+            };
+            for (int i = 0; i < headers.Length; i++)
+            {
+                xlSheet.Cells[i, i + 1] = headers[i];
+            }
+        }
+
+        private void LoadData()
+        {
+            Flats = context.Flats.ToList();  
         }
     }
 }
---
+++ week4_W26E72/week4_W26E72/ModelExcel.Designer.cs
@@ -0,0 +1,10 @@
+﻿// T4 code generation is enabled for model 'C:\Users\Zeke Gábor\Desktop\123\IRF\GIT\week4_W26E72\week4_W26E72\ModelExcel.edmx'. 
+// To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
+// property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
+// is open in the designer.
+
+// If no context and entity classes have been generated, it may be because you created an empty model but
+// have not yet chosen which version of Entity Framework to use. To generate a context class and entity
+// classes for your model, open the model in the designer, right-click on the designer surface, and
+// select 'Update Model from Database...', 'Generate Database from Model...', or 'Add Code Generation
+// Item...'.
