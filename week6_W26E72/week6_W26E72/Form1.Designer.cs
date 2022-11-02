76d57897ac26ed2f32d5f906c451478400faf859
Zeke Gábor <zgabor2001@gmail.com>
2022-11-02 19:56:45 +0100

    Second commit

--- week6_W26E72/week6_W26E72/Form1.Designer.cs
+++ week6_W26E72/week6_W26E72/Form1.Designer.cs
@@ -30,12 +30,43 @@ namespace week6_W26E72
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.createTimer = new System.Windows.Forms.Timer(this.components);
+            this.conveyorTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(13, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(775, 425);
+            this.panel1.TabIndex = 0;
+            // 
+            // createTimer
+            // 
+            this.createTimer.Interval = 3000;
+            // 
+            // conveyorTimer
+            // 
+            this.conveyorTimer.Interval = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer createTimer;
+        private System.Windows.Forms.Timer conveyorTimer;
     }
 }
 
--- week6_W26E72/week6_W26E72/Form1.cs
+++ week6_W26E72/week6_W26E72/Form1.cs
@@ -12,9 +12,44 @@ namespace week6_W26E72
 {
     public partial class Form1 : Form
     {
+        private List<Entities.Ball> _balls = new List<Entities.Ball>();
+        private Entities.BallFactory _factory;
+        public Entities.BallFactory Factory
+        {
+            get { return _factory; }
+            set { _factory = value; }
+        }
         public Form1()
         {
             InitializeComponent();
+            createTimer.Tick += CreateTimer_Tick;
+            conveyorTimer.Tick += ConveyorTimer_Tick;
+        }
+
+        private void ConveyorTimer_Tick(object sender, EventArgs e)
+        {
+            var maxPosition = 0;
+            foreach (var ball in _balls)
+            {
+                ball.MoveBall();
+                if (ball.Left > maxPosition)
+                    maxPosition = ball.Left;
+            }
+
+            if (maxPosition > 1000)
+            {
+                var oldestBall = _balls[0];
+                panel1.Controls.Remove(oldestBall);
+                _balls.Remove(oldestBall);
+            }
+        }
+
+        private void CreateTimer_Tick(object sender, EventArgs e)
+        {
+            var ball = Factory.CreateNew();
+            _balls.Add(ball);
+            ball.Left = -ball.Width;
+            panel1.Controls.Add(ball);
         }
     }
 }
