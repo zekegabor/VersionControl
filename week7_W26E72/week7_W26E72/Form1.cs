8b47732a426728e15699397f4f0d07be31393c57
Zeke Gábor <zgabor2001@gmail.com>
2022-11-09 19:15:55 +0100

    Function insert commit

--- week7_W26E72/week7_W26E72/Form1.cs
+++ week7_W26E72/week7_W26E72/Form1.cs
@@ -31,5 +31,20 @@ namespace week7_W26E72
 
             dataGridView2.DataSource = Portfolio;
         }
+
+        private decimal GetPortfolioValue(DateTime date)
+        {
+            decimal value = 0;
+            foreach (var item in Portfolio)
+            {
+                var last = (from x in Ticks
+                            where item.Index == x.Index.Trim()
+                               && date <= x.TradingDay
+                            select x)
+                            .First();
+                value += (decimal)last.Price * item.Volume;
+            }
+            return value;
+        }
     }
 }
