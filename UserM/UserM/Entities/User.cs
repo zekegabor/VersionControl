b8c281e9bbe22ccf6699127a9425d9d4b3ab92e3
Zeke Gábor <zgabor2001@gmail.com>
2022-11-11 12:02:01 +0100

    Commit fix

---
+++ UserM/UserM/Entities/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserM.Entities
+{
+   public class User
+    {
+        public Guid ID { get; set; } = Guid.NewGuid();
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName
+        {
+            get
+            {
+                return string.Format(
+                    "{0} {1}",
+                    LastName,
+                    FirstName);
+            }
+        }
+
+        // Ugyanaz a FullName property kompaktabb formában is írható
+        /*
+        public string FullName
+            => string.Format(
+                "{0} {1}",
+                LastName,
+                FirstName);
+        */
+    }
+}
