0455c94d0882b9aba394b4ef74f7aea8941f0af5
Zeke Gábor <zgabor2001@gmail.com>
2022-12-09 12:11:19 +0100

    Initial commit

---
+++ week10_W26E72/week10_W26E72/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WorldsHardestGame;
+
+namespace week10_W26E72
+{
+    public partial class Form1 : Form
+    {
+        int populationSize = 100;
+        int nbrOfSteps = 10;
+        int nbrOfStepsIncrement = 10;
+        int generation = 1;
+        GameController gc = new GameController();
+        GameArea ga;
+        public Form1()
+        {
+            InitializeComponent();
+
+            ga = gc.ActivateDisplay();
+            this.Controls.Add(ga);
+
+            gc.AddPlayer();
+            gc.Start(true);
+        }
+    }
+}
